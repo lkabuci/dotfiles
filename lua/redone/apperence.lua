c5067c71162e6e9de904063a1f66d1a7e8e595f7
redouane <elkaboussi@pm.me>
2023-02-10 01:35:04 +0100

    settings

---
+++ lua/redone/apperence.lua
@@ -0,0 +1,22 @@
+-- colorscheme
+vim.o.background = "dark"
+vim.cmd([[colorscheme gruvbox]])
+
+-- indent blanking
+-- vim.opt.list = true
+-- vim.opt.termguicolors = true
+-- vim.cmd [[highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent2 guibg=#1a1a1a gui=nocombine]]
+-- 
+-- require("indent_blankline").setup {
+--     char = "",
+--     char_highlight_list = {
+--         "IndentBlanklineIndent1",
+--         "IndentBlanklineIndent2",
+--     },
+--     space_char_highlight_list = {
+--         "IndentBlanklineIndent1",
+--         "IndentBlanklineIndent2",
+--     },
+--     show_trailing_blankline_indent = false,
+-- }
