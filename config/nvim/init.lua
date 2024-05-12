9ed97eb23880381f4d7c9a159e2ee62fe85996dd
lkabuci <elkaboussi@pm.me>
2024-05-12 11:27:13 +0100

    improve neovim config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,26 +1,5 @@
--- wo Window skipped option
-vim.wo.number = true
-vim.wo.cursorline = true
-
--- Set tabs to be represented as 4 spaces
-vim.bo.tabstop = 4
-vim.bo.shiftwidth = 4
-vim.bo.expandtab = true
-
-
-local options = { noremap = true }
-vim.keymap.set("i", "jk", "<Esc>", options)
-
-vim.o.background = "dark"
-vim.cmd([[colorscheme gruvbox]])
-
--- packer configuration
-return require('packer').startup(function(use)
-
-    use 'wbthomason/packer.nvim' -- Packer
-
-    use { "ellisonleao/gruvbox.nvim" } -- Theme
-
-end)
-
+require ("core.config")
+require ("core.keymaps")
+require ("core.plugins")
+require ("core.plugin_config")
 
